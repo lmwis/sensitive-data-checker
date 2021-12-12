af5c42979d608b58e8adfa777a93729c71c78ab9
lmwis <detectiveygn@163.com>
2021-12-12 15:46:29 +0800

    feat():init

---
+++ src/com/lmwis/data/checker/Main.java
@@ -0,0 +1,13 @@
+package com.lmwis.data.checker;
+
+/**
+ * @Description: TODO
+ * @Author: lmwis
+ * @Data: 2021/12/12 3:45 下午
+ * @Version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
+}
