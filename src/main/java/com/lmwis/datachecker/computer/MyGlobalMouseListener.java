2b3110d8e19324cfc557250e076a37e6ae068c9a
lmwis <detectiveygn@163.com>
2021-12-22 21:07:11 +0800

    mouse

--- src/main/java/com/lmwis/datachecker/computer/MyGlobalMouseListener.java
+++ src/main/java/com/lmwis/datachecker/computer/MyGlobalMouseListener.java
@@ -1,5 +1,6 @@
 package com.lmwis.datachecker.computer;
 
+import com.lmwis.datachecker.mapper.MouseRecord;
 import lombok.extern.slf4j.Slf4j;
 import org.jnativehook.mouse.NativeMouseEvent;
 import org.jnativehook.mouse.NativeMouseInputListener;
@@ -14,16 +15,31 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class MyGlobalMouseListener implements NativeMouseInputListener {
+
+    public static final int MOUSE_RECORD_ACTION_PRESSED=1;
+    public static final int MOUSE_RECORD_ACTION_RELEASED=2;
+    public static final int MOUSE_RECORD_MODE_MOVE=1;
+    public static final int MOUSE_RECORD_MODE_CLICK=2;
+    public static final int MOUSE_RECORD_BUTTON_LEFT=1;
+    public static final int MOUSE_RECORD_BUTTON_RIGHT=2;
+
     public void nativeMouseClicked(NativeMouseEvent e) {
         System.out.println("Mouse Clicked: " + e.getClickCount());
     }
 
     public void nativeMousePressed(NativeMouseEvent e) {
         System.out.println("Mouse Pressed: " + e.getButton());
+
+        MouseRecord mouseRecord = new MouseRecord();
+        mouseRecord.setAction(1);
+        mouseRecord.setMode(1);
     }
 
     public void nativeMouseReleased(NativeMouseEvent e) {
         System.out.println("Mouse Released: " + e.getButton());
+
+        MouseRecord mouseRecord = new MouseRecord();
+        mouseRecord.setAction(2);
     }
 
     public void nativeMouseMoved(NativeMouseEvent e) {
---
+++ src/main/java/com/lmwis/datachecker/mapper/MouseRecord.java
@@ -0,0 +1,58 @@
+package com.lmwis.datachecker.mapper;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.Date;
+
+/**
+ * @Description: TODO
+ * @Author: lmwis
+ * @Data: 2021/12/22 4:00 下午
+ * @Version: 1.0
+ */
+@Data
+@TableName("mouse_record")
+@ToString
+public class MouseRecord {
+
+    @TableId(type = IdType.AUTO)
+    Long id;
+
+    /**
+     * 1-move
+     * 2-click
+     */
+    Integer mode;
+
+    Integer x;
+
+    Integer y;
+
+    /**
+     * click时生效
+     * 枚举值
+     * 按钮类型，左键，右键，中键
+     * 1-左键
+     * 2-右键
+     */
+    Integer button;
+    /**
+     * 1-pressed
+     * 2-released
+     */
+    Integer action;
+
+    /**
+     * 枚举值
+     * mac windows
+     */
+    String pcType;
+
+    Date gmtCreate;
+
+    Date gmtModified;
+}
