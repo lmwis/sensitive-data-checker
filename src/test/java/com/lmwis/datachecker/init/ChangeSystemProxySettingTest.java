4d6be7c34379e5f00ead2641ecd5dbe22d9a3699
lmwis <detectiveygn@163.com>
2022-01-08 17:51:12 +0800

    set wifi proxy auto with runtime package

--- src/test/java/com/lmwis/datachecker/init/ChangeSystemProxySettingTest.java
+++ src/test/java/com/lmwis/datachecker/init/ChangeSystemProxySettingTest.java
@@ -2,6 +2,8 @@ package com.lmwis.datachecker.init;
 
 import org.junit.Test;
 
+import java.io.IOException;
+
 /**
  * @Description: TODO
  * @Author: lmwis
@@ -10,11 +12,57 @@ import org.junit.Test;
  */
 public class ChangeSystemProxySettingTest {
 
+    private static final String[] SET_HTTP_PROXY_COMMAND = {
+            "networksetup",
+            "-setwebproxy",
+            "Wi-Fi",
+            "127.0.0.1",
+            "8888"
+    };
+
+    private static final String[] SET_HTTPS_PROXY_COMMAND ={
+            "networksetup",
+            "-setsecurewebproxy",
+            "Wi-Fi",
+            "127.0.0.1",
+            "8888"
+    };
+    private static final String[] SET_OFF_HTTP_PROXY_COMMAND = {
+            "networksetup",
+            "-setwebproxystate",
+            "Wi-Fi",
+            "off"
+    };
+    private static final String[] SET_OFF_HTTPS_PROXY_COMMAND ={
+            "networksetup",
+            "-setsecurewebproxystate",
+            "Wi-Fi",
+            "off"
+    };
+
     @Test
-    public void doChange(){
+    public void doChange() throws IOException {
         System.out.println(11);
-        //在你发起Http请求之前设置一下属性
-        System.setProperty( "http.proxyHost" ,  "127.0.0.1" );
-        System.setProperty( "http.proxyPort" ,  "8080" );
+
+        Process ls = Runtime.getRuntime().exec(SET_HTTP_PROXY_COMMAND);
+        Runtime.getRuntime().exec(SET_HTTPS_PROXY_COMMAND);
+
+//        Process ls = Runtime.getRuntime().exec("ls");
+//        InputStream inputStream = ls.getInputStream();
+//        IOUtils.readLines(inputStream).stream().forEach(System.out::println);
+
+    }
+
+    @Test
+    public void setOffProxy() throws IOException {
+        System.out.println(11);
+
+        Process ls = Runtime.getRuntime().exec(SET_OFF_HTTP_PROXY_COMMAND);
+        Runtime.getRuntime().exec(SET_OFF_HTTPS_PROXY_COMMAND);
+
+//        Process ls = Runtime.getRuntime().exec("ls");
+//        InputStream inputStream = ls.getInputStream();
+//        IOUtils.readLines(inputStream).stream().forEach(System.out::println);
+
     }
 }
